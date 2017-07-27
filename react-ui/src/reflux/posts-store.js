82bb515cb764a7b750bc3e9a8cb0cf049c8305fb
MorganTrudeau <morgantrudeau@hotmail.com>
2017-07-26 22:41:15 -0700

    folder change

---
+++ react-ui/src/reflux/posts-store.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Reflux from 'reflux';
+import Actions from './actions';
+import axios from 'axios';
+
+var addPost = Actions.addPost;
+var route = Actions.route;
+
+class PostsStore extends Reflux.Store {
+
+  constructor() {
+    super();
+    this.state = {posts: []};
+    this.getState();
+    this.listenTo(addPost, this.onAddPost);
+  }
+
+  getState(data) {
+    axios.get('http://localhost:8888/api/posts')
+      .then(response => {
+        this.setState({posts:response.data});
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onAddPost(text, username) {
+    var config = {
+      headers: {
+        'Accept-Language': 'en-US',
+      }
+    };
+
+    axios.post('http://localhost:8888/api/posts',
+      {"text": text, "username": username}, config)
+        .then(response => {
+          console.log(response);
+          this.getState();
+        }
+    );
+  }
+}
+
+export default PostsStore; 
